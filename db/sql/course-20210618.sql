42e35d6e60c433dfcb0eede57b20a79425ab7ebd
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-06-18 03:36:29 +0800

    1. add nacos config

--- db/sql/course-20210618.sql
+++ db/sql/course-20210618.sql
@@ -1,4 +1,4 @@
 
-alter table course_video change column period_id `period_id` bigint(20) DEFAULT 0 COMMENT 'ID';
+update course_video set period_id=0;
 
 alter table course_video change column `period_id` `period_id` bigint(20) not null DEFAULT 0 COMMENT 'ID';
